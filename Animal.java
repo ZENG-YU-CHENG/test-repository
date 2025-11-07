86489f4242ae28185d928368618fffe42ec68eb2
Andy <yceng5780@gmail.com>
2025-11-07 21:16:53 +0800

    I create two static methods in Animal.java

--- Animal.java
+++ Animal.java
@@ -1,5 +1,14 @@
 class Animal {
   public static void main(String[] args) {
-    System.out.println("Hello, I am a animal");
+    System.out.println(speak());
+    System.out.println(hunt());
+  }
+
+  public static String speak() {
+    return "This animal is speaking";
+  }
+
+  public static String hunt() {
+    return "This animal is hunting";
   }
 }
