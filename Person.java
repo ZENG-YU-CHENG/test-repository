a9fe2946b0474d4005aa524be4b58412d0e40161
Andy <yceng5780@gmail.com>
2025-11-07 21:19:21 +0800

    I create two static methods in Person.java

--- Person.java
+++ Person.java
@@ -1,5 +1,14 @@
 class Person {
   public static void main(String[] args) {
-    System.out.println("Hello, I am a person");
+    System.out.println(speak());
+    System.out.println(work());
+  }
+
+  public static String speak() {
+    return "This person is speaking";
+  }
+
+  public static String work() {
+    return "This person is working";
   }
 }
